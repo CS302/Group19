263a4709de037c81f0ea0ad36bedab8b7a5a081a
CS302 <luckycloud@yandex.ru>
2016-12-19 19:18:12 +0300

    Lesson1
    Lesson1

---
+++ Group19/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Group19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //int number;  //объявление
+            //number = 10; //инициализация
+            //int number2 = 100;
+            //number = 10 * 265 + 6616 - 651;
+            //number = 10 - number;
+            //Console.WriteLine(number);
+
+            //Console.WriteLine();
+            //number = 56;
+            //double price = (double)number / 10;
+            //Console.WriteLine(price);
+
+            //string text = "235";
+            //int number3 = int.Parse(text);
+            //number3 = number3 + 1;
+            //Console.WriteLine(number3);
+
+            //char symbol = 'A';
+            //Console.WriteLine(symbol);
+
+            //Console.WriteLine("a"+"b"+"c");
+            //Console.WriteLine((int)'1');
+
+            //bool flag = true;
+            //flag = true && false;
+            //Console.WriteLine(!flag);
+
+            //int number = 10;
+            //number += 2;
+            //number = number + 2;
+            DateTime date = new DateTime(2016, 12, 19);
+            Console.WriteLine(date.DayOfWeek);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("4865132");
+            
+
+        }
+    }
+}
